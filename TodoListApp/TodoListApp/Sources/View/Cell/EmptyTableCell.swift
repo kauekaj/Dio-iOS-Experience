e40303ace54bf06b7f46baaaa1422602fc0f61ea
Kaue Jacyntho <kauekaj@gmail.com>
2022-12-08 14:14:21 -0300

    Create EmptyCell

---
+++ TodoListApp/TodoListApp/Sources/View/Cell/EmptyTableCell.swift
@@ -0,0 +1,21 @@
+//
+//  EmptyTableCell.swift
+//  TodoListApp
+//
+//  Created by Kaue de Assis Jacyntho on 08/12/22.
+//
+
+import UIKit
+
+class EmptyTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
