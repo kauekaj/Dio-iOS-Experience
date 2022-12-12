8f5e1ff506f8029bdbc96ae264a1ccc1c148e350
Kaue Jacyntho <kauekaj@gmail.com>
2022-12-12 11:27:11 -0300

    create extension Date

---
+++ TodoListApp/TodoListApp/Sources/Extensions/Date.swift
@@ -0,0 +1,31 @@
+//
+//  Date.swift
+//  TodoListApp
+//
+//  Created by Kaue de Assis Jacyntho on 12/12/22.
+//
+
+import Foundation
+
+extension Date {
+    
+    public func convertdateToString(date: Date, dateFormatter: String?) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-dd'T'HH:mm:ss"
+        let myString = formatter.string(from: date)
+        let yourDate = formatter.date(from: myString)
+        
+        formatter.dateFormat = dateFormatter ?? "yyyy-dd'T'HH:mm:ss"
+        return formatter.string(from: yourDate!)
+    }
+    
+    
+    public func convertStringToDate(dateString: String, dateFormatter: String?) -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = dateFormatter ?? "yyyy-dd'T'HH:mm:ss"
+        formatter.timeZone = TimeZone.current
+        formatter.locale = Locale.current
+        return formatter.date(from: dateString) ?? nil
+     
+    }
+}
