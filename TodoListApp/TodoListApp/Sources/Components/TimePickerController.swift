ddd545b8c1d90bca369e85ce8dac6ed885c05a86
Kaue Jacyntho <kauekaj@gmail.com>
2022-12-12 11:10:39 -0300

    Implemented datePicker to configure Tasks

---
+++ TodoListApp/TodoListApp/Sources/Components/TimePickerController.swift
@@ -0,0 +1,44 @@
+//
+//  TimePickerController.swift
+//  TodoListApp
+//
+//  Created by Kaue de Assis Jacyntho on 12/12/22.
+//
+
+import UIKit
+
+protocol TimePickerProtocol {
+    func sendTime(time: String)
+}
+
+class TimePickerController: UIViewController {
+
+    //MARK: IBOutlet
+    @IBOutlet weak var btnCancel: UIButton!
+    @IBOutlet weak var btnOK: UIButton!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    //MARK: Var/Let
+    
+    public var delegate: TimePickerProtocol?
+
+    
+    //MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close(_ sender: UIButton) {
+        if sender == self.btnOK {
+            self.dismiss(animated: true) {
+                guard let delegate = self.delegate else { return }
+                delegate.sendTime(time: "03:34")
+            }
+        } else {
+            self.dismiss(animated: true)
+        }
+    }
+    
+
+}
--- TodoListApp/TodoListApp/Sources/Controller/TaskManagerTableViewController.swift
+++ TodoListApp/TodoListApp/Sources/Controller/TaskManagerTableViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import FSCalendar
 
 class TaskManagerTableViewController: UITableViewController {
 
+    @IBOutlet weak var btnTitle: UITextField!
+    @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var btnTime: UIButton!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let controller = segue.destination as? TimePickerController {
+            controller.delegate = self
+        }
+    }
+    
+    
+    //MARK: - IBAction
+    @IBAction func goBack(_ sender: Any) {
+        
+    }
+    
+    @IBAction func openComponent(_ sender: Any) {
+        self.performSegue(withIdentifier: "segueComponentTime", sender: nil)
+    }
+    
+    @IBAction func deleteTask(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func createTask(_ sender: Any) {
+        
+    }
+
+}
+
+extension TaskManagerTableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -23,7 +57,12 @@ class TaskManagerTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
+}
 
-  
-
+extension TaskManagerTableViewController: TimePickerProtocol {
+    func sendTime(time: String) {
+        self.btnTime.setTitle(time, for: .normal)
+    }
+    
+    
 }
